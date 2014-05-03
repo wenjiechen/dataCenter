1888fdf4c0ba9206be0ea0aafedaef6b3727d800
wenjie chen <wenjie.chen@nyu.edu>
2014-05-03 00:41:32 -0400

    load model from csv file

--- Models.py
+++ Models.py
@@ -5,7 +5,7 @@ class Switch(object):
     __metaclass__ = abc.ABCMeta
 
     def __init__(self,ID):
-        self.ID = ID
+        self.ID = int(ID)
         self.switch_table = []
 
     @abc.abstractmethod
@@ -24,6 +24,12 @@ class Spine(Switch):
     def __str__(self):
         return 'Spine_'+str(self.ID)
 
+    def __eq__(self, other):
+        return isinstance(other, Spine) and self.ID == other.ID
+
+    def __hash__(self):
+        return hash('spine') + hash(17 + self.ID)
+
 class Leaf(Switch):
     def update_switch_table(self):
         print'Leaf'
@@ -31,24 +37,74 @@ class Leaf(Switch):
     def __str__(self):
         return 'Leaf_'+str(self.ID)
 
+    def __eq__(self, other):
+        return isinstance(other, Leaf) and self.ID == other.ID
+
+    def __hash__(self):
+        return hash('leaf') + hash(17 + self.ID)
+
 class Host(object):
 
-    def __init__(self,rack_num,ID,MAC=None,IP=None):
-        self.rack_num = rack_num
-        self.ID = ID
-        self.MAC = MAC
-        self.IP = IP
+    def __init__(self,rack_id,ID,MACs=None,IPs=None):
+        self.rack_id = int(rack_id)
+        self.ID = int(ID)
+        self.MACs = MACs
+        self.IPs = IPs
 
     def __str__(self):
-        return 'Host_' + str(self.ID) + ', Rack_' + str(self.rack_num) + ', MAC: '+str(self.MAC) + ', IP: ' + str(self.IP)
+        # return 'Host_' + str(self.ID) + ', Rack_' + str(self.rack_num) + ', MACs: '+str(self.MACs) + ', IPs: ' + str(self.IPs)
+        return 'Host_' + str(self.ID) + '@Rack_' + str(self.rack_id)
 
     def __repr__(self):
         return self.__str__()
 
-if __name__=='__main__':
+    def __eq__(self, other):
+        return isinstance(other, Host) and self.ID == other.ID and self.rack_id == other.rack_id
+
+    def __hash__(self):
+        return hash(17 + self.ID*31 + self.rack_id)
+
+class Rack(object):
+
+    def __init__(self,ID):
+        self.ID = int(ID)
+        self.hosts = []
+
+    def add_host(self,host):
+        self.hosts.append(host)
+
+    def __str__(self):
+        return 'Rack_' + str(self.ID)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return isinstance(other, Rack) and self.ID == other.ID
+
+    def __hash__(self):
+        return hash(17 + self.ID)
+
+def factory(type, ID):
+    if type == "Spine":
+        return Spine(ID)
+    elif type == "Leaf":
+        return Leaf(ID)
+    elif type == "Host":
+        rack_id, host_id = ID.split('_')
+        host = Host(rack_id[1:],host_id)
+        return host
+    elif type == "Rack":
+        return Rack(ID)
+    else:
+        raise TypeError("Don't have this type")
+
+def test3():
     s1 = Spine(1)
-    print str(s1)
-    l1 = Leaf(1)
-    print l1
-    h1 = Host(1,1)
-    print h1
+    s2 = Spine(2)
+    d = {s1:100, s2:200}
+    print d[Spine(1)]
+    print d[factory('Spine',2)]
+
+if __name__=='__main__':
+    test3()
---
+++ datacenter.py
@@ -0,0 +1,49 @@
+__author__ = 'wenjie'
+from Models import Host, Spine, Leaf, Rack
+import Models
+import networkx as nx
+import csv
+
+class DataCenter(object):
+
+    def __init__(self, name='test'):
+        self.name = name
+        self._datacenter_model = nx.Graph()
+
+    def _add_edge(self, args):
+        type1, id1, port1, type2, id2, port2, link_speed = args
+        node1 = Models.factory(type1,id1)
+        node2 = Models.factory(type2,id2)
+        self._datacenter_model.add_edge(node1,node2)
+        if isinstance(node1,Host):
+            rack = Models.factory('Rack',node1.rack_id)
+            self._datacenter_model.add_node(rack)
+
+    def load_csv_model_by_edges(self, file_path, delimiter=','):
+        with open(file_path,'rb') as csv_model:
+            reader = csv.reader(csv_model,delimiter=delimiter)
+            # skip the headers
+            next(reader, None)
+            for row in reader:
+                # eliminate whitespace in words
+                self._add_edge([e.strip() for e in row])
+
+    def load_hosts_attributes(self,file_path):
+        pass
+
+
+if __name__=='__main__':
+    dc = DataCenter()
+    file_path = 'testModel.csv'
+    file_path2 = 'dcModel2.csv'
+    dc.load_csv_model_by_edges(file_path2)
+    print sorted(dc._datacenter_model.nodes())
+    print dc._datacenter_model.neighbors(Spine(1))
+    print dc._datacenter_model.neighbors(Leaf(1))
+    print dc._datacenter_model.neighbors(Host(1,1))
+
+
+
+
+
+
